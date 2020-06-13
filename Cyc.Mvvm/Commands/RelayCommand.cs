472514f2dda8c866931a90b59ca7e653a8a6dba4
Alex Chuang <1392alex1392@gmail.com>
2020-06-13 05:11:09 +0100

    add relayCommand

---
+++ Cyc.Mvvm/Commands/RelayCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Cyc.Mvvm.Commands {
+	/// <summary>
+	/// A command which can always be executed.
+	/// </summary>
+	public class RelayCommand : ICommand {
+		private readonly Action action;
+
+		public event EventHandler CanExecuteChanged;
+
+		public RelayCommand(Action action)
+		{
+			this.action = action;
+		}
+
+		public bool CanExecute(object parameter)
+		{
+			return true;
+		}
+
+		public void Execute(object parameter)
+		{
+			action?.Invoke();
+		}
+	}
+}
